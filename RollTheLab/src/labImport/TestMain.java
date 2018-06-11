57184e46df187807a1eb82ea24add45ae298e377
Jonas Blaschek <jonasblaschek.jb@gmail.com>
2018-06-11 22:05:25 +0200

    dfs

--- RollTheLab/src/labImport/TestMain.java
+++ RollTheLab/src/labImport/TestMain.java
@@ -1,7 +1,7 @@
 package labImport;
 
 public class TestMain {
-	//hikösdf
+	//hikösdfjoans
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ImageReader imageReader = new ImageReader("D:\\Projekte\\Java\\Git Rep\\RollTheLab\\src\\labImport\\TestImage1.jpg");
