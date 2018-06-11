85b8260e2e9cc571ef6003e45a4fa88b393b377a
Jonas Blaschek <jonasblaschek.jb@gmail.com>
2018-06-11 22:07:34 +0200

    Ready

--- RollTheLab/src/labImport/TestMain.java
+++ RollTheLab/src/labImport/TestMain.java
@@ -1,7 +1,6 @@
 package labImport;
 
 public class TestMain {
-	//hikösdfjoans
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ImageReader imageReader = new ImageReader("D:\\Projekte\\Java\\Git Rep\\RollTheLab\\src\\labImport\\TestImage1.jpg");
